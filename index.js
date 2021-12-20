f2ca82c85a4db23cbd97081c036f400937ecd4f3
claudia welch <claudia.welch73@gmail.com>
2021-12-21 09:24:49 +1100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(drivers){
+    const firstDrivers = drivers.slice(0,2);
+    return firstDrivers;
+}
+returnFirstTwoDrivers(drivers)
+
+function returnLastTwoDrivers(drivers){
+    const lastDrivers = drivers.slice(2,4);
+    return lastDrivers;
+}
+returnLastTwoDrivers(drivers)
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function fareDoubler(n){
+    return n*2;
+}
+
+function fareTripler(n){
+    return n*3;
+}
+
+function selectDifferentDrivers(arrayOfDrivers, selectDriver ) {
+    return selectDriver(arrayOfDrivers);
+
+}
+
+selectDifferentDrivers(drivers,returnFirstTwoDrivers)
+
+function createFareMultiplier(fare){
+ 
+        return function createFareMultiplier(){
+            const fareQuintupler = 5;
+            return fareQuintupler*fare;
+        }
+           
+       
+      
+}
